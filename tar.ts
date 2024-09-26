c7d50c689dab4f48f0852a2039ed280a8d379306
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2024-09-26 04:09:24 -0700

    chore: update package-lock.json

---
+++ tar.ts
@@ -0,0 +1,75 @@
+import { createWriteStream, statSync } from "fs";
+import globby from "globby";
+import archiver from "archiver";
+import { finished } from "stream/promises";
+
+const cache = async () => {
+  const globs = ["./node_modules/**/*"];
+  const root = "/home/ANT.AMAZON.COM/kgnanask/sandbox/create-react-app";
+  const cacheFileName = "cache.tar";
+  const cacheArchiveWriteStream = createWriteStream(`${root}/${cacheFileName}`);
+
+  cacheArchiveWriteStream.on("error", (err) => {
+    console.error("Error writing cache archive stream", err);
+  });
+
+  const tarArchiver = archiver("tar");
+
+  // Register error listener for logging alone
+  tarArchiver.on("error", (err) => {
+    console.error("Failed to process compressed file", err);
+  });
+
+  tarArchiver.pipe(cacheArchiveWriteStream);
+
+  cacheArchiveWriteStream.on("close", () => {
+    console.log("Close event emitted for cache archive write stream");
+  });
+
+  cacheArchiveWriteStream.on("end", () => {
+    console.log("End event emitted for cache archive write stream");
+  });
+
+  cacheArchiveWriteStream.on("finish", () => {
+    console.log("Finish event emitted for cache archive write stream");
+  });
+
+  const globStream = globby.stream(globs, {
+    dot: true,
+    cwd: root,
+    deep: 20,
+  });
+
+  // Append each file from the spec to the zip archive
+  console.log("# Creating cache artifact...");
+
+  for await (const path of globStream) {
+    const stats = statSync(`${root}/${path}`);
+
+    console.log(`Adding resource to cache archive`, {
+      path,
+      isSymbolicLink: stats.isSymbolicLink(),
+      isDirectory: stats.isDirectory(),
+      isFile: stats.isFile(),
+      size: stats.size,
+    });
+
+    tarArchiver.file(`${root}/${path}`, {
+      name: path.toString(),
+      // stats,
+    });
+  }
+
+  await tarArchiver.finalize();
+
+  // Wait for the stream to close
+  await finished(cacheArchiveWriteStream);
+};
+
+cache()
+  .then(() => {
+    console.log("Cache artifact created successfully");
+  })
+  .catch((err) => {
+    console.error("Failed to create cache artifact", err);
+  });
